3a1b178ecfd6748b2cf94b59af85ee153d935ff9
AayushKotwani3 <aayushkotwani9@gmail.com>
2025-03-25 22:33:06 +0530

    Added comments for Number guesser game

--- Number Guesser Game/number_guesser.py
+++ Number Guesser Game/number_guesser.py
@@ -1,5 +1,6 @@
-import random
+import random # Importing the random module to generate random numbers
 
+# Taking user input for the top limit of the range and handling invalid inputs
 try:
     top_of_range = int(input("Enter a number that will be your top of range: "))  # Try converting input to an integer
     if top_of_range <= 0:
@@ -9,8 +10,10 @@ except ValueError:
     print("Please enter a valid number next time.")
     quit()
 
+# Generating a random number within the user-specified range
 random_number=random.randint(0,top_of_range)
 guesses=0
+# Loop for user to guess the number until the correct guess
 while True:
     guesses +=1
     try:
@@ -18,7 +21,7 @@ while True:
     except ValueError:
         print("Please enter a valid number next time.")
         continue
-
+    # Checking if the guess is correct, too high, or too low
     if user_guess==random_number:
         print("You got it right !")
         break
@@ -26,6 +29,6 @@ while True:
         print("wrong! your guess is above the number")
     else:
         print("wrong! your guess is below the number")
-
+# Displaying the total number of attempts
 print("you guesseed it in",guesses,"guesses")
 
